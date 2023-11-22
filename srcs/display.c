b88025992adb40ba5aa7a710847590757f48b507
ts <ts@ts.com>
2023-11-22 14:06:11 -0600

    verbose OK with address as str

--- srcs/display.c
+++ srcs/display.c
@@ -31,23 +31,43 @@ static unsigned int    get_offset(unsigned int frag_off)
     return offset;
 }
 
+char    *addr_to_str(int addr)
+{
+    char res[16];
+    
+    sprintf(res, "%u.%u.%u.%u", addr & 0xFF, (addr >> 8) & 0xFF, (addr >> 16) & 0xFF, (addr >> 24) & 0xFF);
+    return ft_strdup(res);
+}
+
+static void    display_ip_header(unsigned char *bytes_ip)
+{
+    struct iphdr    *ip = (struct iphdr *)(bytes_ip);
+
+    printf("IP Hdr Dump:\n");
+    for (int i = 0; i < IP_HEADER_LEN; i = i + 2)
+        printf(" %02X%02X", bytes_ip[i], bytes_ip[i + 1]);
+    printf("\n");
+    printf("%2s %2s %3s %4s %4s %3s %4s %3s %3s %4s %12s %12s %s\n", "Vr","HL","TOS","Len","ID","Flg","off","TTL","Pro","cks","Src","Dst","Data"); // TO DO why is Data empty
+    printf("%2x %2x %3x %04x %4x %3x %04x %3x %3x %4x %12s %12s\n", ip->version, ip->ihl, ip->tos, htons(ip->tot_len), ip->id, get_flags(ip->frag_off), get_offset(ip->frag_off), ip->ttl, ip->protocol, htons(ip->check), addr_to_str(ip->saddr), addr_to_str(ip->daddr));
+
+}
+
+static void    display_icmp_header(t_data *dt, unsigned char *bytes)
+{
+    struct icmphdr  *icmp_in_payload = (struct icmphdr *)(bytes + IP_HEADER_LEN + ICMP_HEADER_LEN);
+
+    printf("ICMP: type %d, code %d, size %d, id 0x%04x, seq 0x%04d\n", icmp_in_payload->type, icmp_in_payload->code, dt->one_seq.bytes, htons(icmp_in_payload->un.echo.id), icmp_in_payload->un.echo.sequence);
+}
+
 void    display_ping_unreachable(t_data *dt)
 {
     printf("%d bytes from %s: Destination Host Unreachable\n", dt->one_seq.bytes, dt->resolved_address);
     debug_packet(&dt->one_seq.final_packet);
     if (dt->options_params.v)
     {
-        const unsigned char *bytes = (const unsigned char *)dt->one_seq.final_packet.icmp;
-        const unsigned char *bytes_ip = bytes + ICMP_HEADER_LEN;
-        struct iphdr    *ip = (struct iphdr *)(bytes_ip);
-        struct icmphdr  *icmp_in_payload = (struct icmphdr *)(bytes + IP_HEADER_LEN + ICMP_HEADER_LEN);
-        printf("IP Hdr Dump:\n");
-        for (int i = 0; i < IP_HEADER_LEN; i = i + 2)
-            printf(" %02X%02X", bytes_ip[i], bytes_ip[i + 1]);
-        printf("\n");
-        printf("%2s %2s %3s %4s %4s %3s %4s %3s %3s %4s %8s %8s %s\n", "Vr","HL","TOS","Len","ID","Flg","off","TTL","Pro","cks","Src","Dst","Data");
-        printf("%2x %2x %3x %04x %4x %3x %04x %3x %3x %4x %08x %08x\n", ip->version, ip->ihl, ip->tos, htons(ip->tot_len), ip->id, get_flags(ip->frag_off), get_offset(ip->frag_off), ip->ttl, ip->protocol, htons(ip->check), ip->saddr, ip->daddr); // TO DO addresses as ascii
-        printf("ICMP: type %d, code %d, size %d, id 0x%04x, seq 0x%04d\n", icmp_in_payload->type, icmp_in_payload->code, dt->one_seq.bytes, htons(icmp_in_payload->un.echo.id), icmp_in_payload->un.echo.sequence);
+        unsigned char *bytes = (unsigned char *)dt->one_seq.final_packet.icmp;
+        display_ip_header(bytes + ICMP_HEADER_LEN);
+        display_icmp_header(dt, bytes);
     }
 }
 
